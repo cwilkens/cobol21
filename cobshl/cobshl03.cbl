e6fe0229601fa44bd54ab724de1eb54dcc2f5edb
cwilkens <scavengerq@gmail.com>
2021-01-26 17:06:49 -0600

    New cobol shell program.

---
+++ cobshl/cobshl03.cbl
@@ -0,0 +1,20 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBSHL03.
+      *--------------------------------------------
+      * Comments go here.
+      *--------------------------------------------
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+
+       PROGRAM-DONE.
+           STOP RUN.
+           
