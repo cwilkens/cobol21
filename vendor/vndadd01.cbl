788c6dd520bd302cc9e973c1c58ff97777f6f17a
cwilkens <scavengerq@gmail.com>
2021-01-26 18:01:01 -0600

    Check if a vendor number exists before adding it.
    Listing 13.4.

---
+++ vendor/vndadd01.cbl
@@ -0,0 +1,116 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. VNDADD01.
+      *----------------------------------------------
+      * Add a record to an indexed Vendor File.
+      *----------------------------------------------
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           
+           COPY "slvnd01.cbl".
+
+       DATA DIVISION.
+       FILE SECTION.
+       
+           COPY "fdvnd02.cbl".
+           
+       WORKING-STORAGE SECTION.
+
+       01  RECORD-FOUND                        PIC X.
+       01  VENDOR-NUMBER-FIELD                 PIC Z(5).
+
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+           OPEN I-O VENDOR-FILE.
+           PERFORM GET-NEW-VENDOR-NUMBER.
+           PERFORM ADD-RECORDS
+               UNTIL VENDOR-NUMBER = ZEROES.
+           CLOSE VENDOR-FILE.
+
+       PROGRAM-DONE.
+           STOP RUN.
+           
+       GET-NEW-VENDOR-NUMBER.
+           PERFORM INIT-VENDOR-RECORD.
+           PERFORM ENTER-VENDOR-NUMBER.
+           MOVE "Y" TO RECORD-FOUND.
+           PERFORM FIND-NEW-VENDOR-RECORD
+               UNTIL RECORD-FOUND = "N" OR
+                     VENDOR-NUMBER = ZEROES.
+
+       INIT-VENDOR-RECORD.
+           MOVE SPACE TO VENDOR-RECORD.
+           MOVE ZEROES TO VENDOR-NUMBER.
+
+       ENTER-VENDOR-NUMBER.
+           DISPLAY " ".
+           DISPLAY "ENTER VENDOR NUMBER OF THE VENDOR".
+           DISPLAY "TO ADD (1-99999)".
+           DISPLAY "ENTER 0 TO STOP ENTRY".
+           ACCEPT VENDOR-NUMBER-FIELD.
+           MOVE VENDOR-NUMBER-FIELD TO VENDOR-NUMBER.
+
+       FIND-NEW-VENDOR-RECORD.
+           PERFORM READ-VENDOR-RECORD.
+           IF RECORD-FOUND = "Y"
+               DISPLAY "RECORD ALREADY ON FILE"
+               PERFORM ENTER-VENDOR-NUMBER.
+
+       READ-VENDOR-RECORD.
+           MOVE "Y" TO RECORD-FOUND.
+           READ VENDOR-FILE RECORD
+             INVALID KEY
+               MOVE "N" TO RECORD-FOUND.
+
+               
+       ADD-RECORDS.
+           PERFORM ENTER-REMAINING-FIELDS.
+           PERFORM WRITE-VENDOR-RECORD.
+           PERFORM GET-NEW-VENDOR-NUMBER.
+
+       WRITE-VENDOR-RECORD.
+           WRITE VENDOR-RECORD
+               INVALID KEY
+               DISPLAY "RECORD ALREADY ON FILE".
+
+       ENTER-REMAINING-FIELDS.
+           PERFORM ENTER-VENDOR-NAME.
+           PERFORM ENTER-VENDOR-ADDRESS-1.
+           PERFORM ENTER-VENDOR-ADDRESS-2.
+           PERFORM ENTER-VENDOR-CITY.
+           PERFORM ENTER-VENDOR-STATE.
+           PERFORM ENTER-VENDOR-ZIP.
+           PERFORM ENTER-VENDOR-CONTACT.
+           PERFORM ENTER-VENDOR-PHONE.
+
+       ENTER-VENDOR-NAME.
+           DISPLAY "ENTER VENDOR NAME".
+           ACCEPT VENDOR-NAME.
+
+       ENTER-VENDOR-ADDRESS-1.
+           DISPLAY "ENTER VENDOR ADDRESS-1".
+           ACCEPT VENDOR-ADDRESS-1.
+
+       ENTER-VENDOR-ADDRESS-2.
+           DISPLAY "ENTER VENDOR ADDRESS-2".
+           ACCEPT VENDOR-ADDRESS-2.
+
+       ENTER-VENDOR-CITY.
+           DISPLAY "ENTER VENDOR CITY".
+           ACCEPT VENDOR-CITY.
+
+       ENTER-VENDOR-STATE.
+           DISPLAY "ENTER VENDOR STATE".
+           ACCEPT VENDOR-STATE.
+
+       ENTER-VENDOR-ZIP.
+           DISPLAY "ENTER VENDOR ZIP".
+           ACCEPT VENDOR-ZIP.
+
+       ENTER-VENDOR-CONTACT.
+           DISPLAY "ENTER VENDOR CONTACT".
+           ACCEPT VENDOR-CONTACT.
+
+       ENTER-VENDOR-PHONE.
+           DISPLAY "ENTER VENDOR PHONE".
+           ACCEPT VENDOR-PHONE.
